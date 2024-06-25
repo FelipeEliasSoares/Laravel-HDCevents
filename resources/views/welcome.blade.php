41e902a04e78acf76932914a5b03831574f37a07
FelipeEliasSoares <soaresfelipe396@gmail.com>
2024-06-25 20:34:17 -0300

    Aula 14

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,32 @@
 @section('title', 'HDC Events')
 
 @section('content')
+<div class="container-fluid">
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
 
-@foreach($events as $event)
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle"> Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+        <div class="card col-md-3">
+            <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+            <div class="card-body">
+                <p class="card-date">25/06/2024</p>
+                <h5 class="card-title"> {{ $event->title }} </h5>
+                <p class="card-participants"> X Participantes </p>
+                <a href="#" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+        @endforeach
+</div>
+</div>
 
-    <p>{{ $event->title }} -- {{ $event->description }}</p>
 
-@endforeach
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,3 @@ Route::get('/events/create', [EventController::class, 'create']);
 
 Route::get('/contato', [EventController::class, 'contact']);
 
-
-Route::get('/produtos', [EventController::class, 'produtos']);
-
-Route::get('/produtos_teste/{id?}', [EventController::class, 'produtos_teste']);
